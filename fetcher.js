c0e118d2c3085289d178e93c9b8656c232e4f235
Ritaben Vaghasia <ritakanani@yahoo.com>
2021-12-04 16:54:45 -0500

    request http

---
+++ fetcher.js
@@ -0,0 +1,23 @@
+const fs = require('fs');   // index.html file
+const request = require('request');    // htttp request file
+
+const originalNum = process.argv.slice(2);
+console.log(originalNum);
+
+request(originalNum[0], function(error, response, body) {
+  console.log(originalNum);
+  console.log(error);
+  console.log(response);
+  console.log(body);
+
+  fs.writeFile(originalNum[1], body, err => {
+    if (err) {
+      console.error(err);
+      return
+    }
+    //file written successfully
+  });
+
+});
+// https://www.npmjs.com/package/request
+
